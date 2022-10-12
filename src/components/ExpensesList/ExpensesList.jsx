fb35b14ab0096c39f3411ed0325127de93786717
Silas Henrique <silash.silva00@gmail.com>
2022-10-11 22:36:02 -0300

    feat(): new component - ExpensesList

---
+++ src/components/ExpensesList/ExpensesList.jsx
@@ -0,0 +1,23 @@
+import ExpenseItem from "../ExpenseItem/ExpenseItem";
+import "./ExpensesList.css";
+
+const ExpenseList = (props) => {
+  if (props.items.length == 0) {
+    return <h2 className="expenses-list__fallback">No Expenses Found.</h2>;
+  }
+
+  return (
+    <ul className="expenses-list">
+      {props.items.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default ExpenseList;
